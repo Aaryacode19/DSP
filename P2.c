42bf3fe05778e7ecdee3f3f62d43d8d07563cb60
Aarya <152620731+Aaryacode19@users.noreply.github.com>
2024-11-21 07:14:47 +0530

    Update P2.c

--- P2.c
+++ P2.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
-int custom_len_funcn(char str[])
+void custom_strcpy(char s1[], char s2[]) {
+    int i = 0;
+    while (s2[i] != '\0') {
+        s1[i] = s2[i]; // Copy character from s2 to s1
+        i++;
+    }
+    s1[i] = '\0'; // Add null terminator to the end of s1
+}
+
+
+int custom_len(char str[])
 {
     int length = 0;
     while(str[length] != '\0')
     {
-        length ++;
+        length++;
     }
     return length;
 }
 
-int custom_cmp_funcn(char s1[], char s2[])
+int custom_cmp(char s1[], char s2[])
 {
     int i = 0;
     while(s1[i] == s2[i])
@@ -24,25 +35,16 @@ int custom_cmp_funcn(char s1[], char s2[])
     return s1[i] - s2[i];
 }
 
-void custom_cpy_funcn(char s1[], char s2[])
-{
-    int i = 0;
-    while((s1[i] = s2[i]) != '\0')
-    {
-     i++;   
-    }
-}
-
 int main(void)
 {
     char name[] = "Aarya";
-    printf("lenght of name is: %d", custom_len_funcn(name));
+    printf("lenght of name is: %d", custom_len(name));
     
     char s1[100] = "Hey";
     char s2[100];
-    custom_cpy_funcn(s2, s1);
+    custom_strcpy(s2, s1);
     printf("\n %s", s2);
     
-    int result = custom_cmp_funcn(s1, s2);
+    int result = custom_cmp(s1, s2);
     printf("\n %d", result);
 }

0d80cfe8439bd1bac0abfc6b707fc803c5fcf043
Aarya <152620731+Aaryacode19@users.noreply.github.com>
2024-11-24 07:09:56 +0530

    Create P1.py

---
+++ P1.py
@@ -0,0 +1,26 @@
+#Create variables of different data types (integer, float, string, Boolean).
+#Perform basic operations on them and print the results along with their types using the type () function.
+
+var_int = 19
+var_float = 19.9
+var_string = "Hi Buddy"
+var_boolean = True
+
+#basic operations
+addn = var_int + int(var_float)
+print(addn)
+
+sub = var_int - 1
+print(sub)
+
+mul = var_float * 2
+print(mul)
+
+div = var_int / 2
+print(div)
+
+bool = not var_boolean
+print(bool)
+
+st_conc = f"{var_string}, im aarya"
+print(st_conc)

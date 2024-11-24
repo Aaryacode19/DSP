2dbf3d452c82e6e95bc82e40644d299b6278c7bf
Aarya <152620731+Aaryacode19@users.noreply.github.com>
2024-11-24 08:04:44 +0530

    Create P10.py

---
+++ P10.py
@@ -0,0 +1,20 @@
+import numpy as np
+#creating two numpy arrays
+array1 = np.array([4, 9, 16, 25])
+print(f"Array 1: {array1}")
+array2 = np.array([1, 2, 4, 5])
+print(f"Array 2: {array2}")
+addition = array1 + array2 #addn of two numpy arrays
+print(f"Addition of two arrays: {addition}")
+subtraction = array1 - array2 #sub of two numpy arrays
+print(f"Subtraction of two arrays: {subtraction}")
+multiplication = array1 * array2 #multi of two numpy arrays
+print(f"Multiplication of two arrays: {multiplication}")
+division = array1 / array2 #division of two numpy arrays
+print(f"Division of two arrays: {division}")
+sqrt_array1 = np.sqrt(array1) #sq root of array 1
+print(f"Square Root of Array 1: {sqrt_array1}")
+log_array2 = np.log(array2) #logarithm (base e) of array2
+print(f"Logarithm of Array 2: {log_array2}")
+exp_array1 = np.exp(array1) #exponential of array1
+print(f"Exponential of Array 1: {exp_array1}")
